c80b8f46dfd239b137ebed06f3d08cf26fa833bb
Vladislav <vetuhvladislav@gmail.com>
2026-09-16 15:19:07 -0700

    Declare Compose saveable runtime dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.runtime:runtime-saveable")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
