5d5fd7be2cfd259296a3a04b744d263eada50a44
Vladislav <vetuhvladislav@gmail.com>
2026-09-16 23:28:58 -0700

    Clean preset store imports

--- app/src/main/java/dev/deepagent/mobile/agent/preset/AgentPresetStore.kt
+++ app/src/main/java/dev/deepagent/mobile/agent/preset/AgentPresetStore.kt
@@ -13,7 +13,6 @@ import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 import javax.crypto.spec.GCMParameterSpec
-import javax.crypto.spec.SecretKeySpec
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 
