8154f723adf5d464572ff7c1806a336b0739d5d1
Vladislav <vetuhvladislav@gmail.com>
2026-09-18 08:58:34 -0700

    Bump app version to 0.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.deepagent.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.5"
+        versionCode = 6
+        versionName = "0.1.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
